c5aa07e1f85c3b5ae2408787ba1f2327551b631f
Adriano Oliviero <adrianoliviero23@gmail.com>
2025-12-05 12:12:02 +0100

    Initial Commit

---
+++ CamShot/CamShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  CamShotApp.swift
+//  CamShot
+//
+//  Created by Adriano Oliviero on 05/12/25.
+//
+
+import SwiftUI
+
+@main
+struct CamShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CamShot/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CamShot
+//
+//  Created by Adriano Oliviero on 05/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
